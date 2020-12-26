4c74dbde97f0db9f96527559c2f80a304fc4038d
scott-Tresor <34010260+Scott-Tresor@users.noreply.github.com>
2020-12-26 12:19:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### Hello There 👋
+
+![](https://github-readme-stats.vercel.app/api?username=eric-ampire&show_icons=true&count_private=true)
+
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=eric-ampire&layout=compact)
+
+To get started:
+
+- I’m currently working on ...
+- I’m currently learning ...
+- I’m looking to collaborate on ...
+- Want to reach out: [www.ericampire.xyz](www.ericampire.xyz)
---
+++ pages/_lang/contact.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="container">
+    <Layout>
+      <h2 slot="header" class="has-text-white">{{$t('links.contact')}}</h2>
+      <div slot="content">
+        <div class="columns">
+          <div class="column">
+            <table>
+              <tr>
+                <td>
+                  <div class="contact-icon">
+                    <i class="fa fa-map-marker"></i>
+                  </div>
+                </td>
+                <td>Rwanda</td>
+              </tr>
+              <tr>
+                <td>
+                  <div class="contact-icon">
+                    <i class="fa fa-envelope"></i>
+                  </div>
+                </td>
+                <td>oesukam@gmail.com</td>
+              </tr><tr>
+                <td>
+                  <div class="contact-icon">
+                    <i class="fa fa-phone"></i>
+                  </div>
+                </td>
+                <td>+250-783-200-211</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  </section>
+</template>
+
+<script>
+import Layout from '~/components/PageLayout'
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style scoped>
+  .contact-icon {
+    background-color: #fafafa;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 20px;
+  }
+  tr {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
